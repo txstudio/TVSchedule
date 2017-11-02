97ec750c14849875c82e6743c36c2894e0a56f55
Shi Hao Wang <st890409@hotmail.com>
2017-11-02 11:22:24 +0800

    下載網頁內容類別

---
+++ ScheduleDownloader/WebPageDownloader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ScheduleDownloader
+{
+    public class WebPageDownloader
+    {
+        protected HttpClient _client;
+
+        public WebPageDownloader()
+        {
+            this._client = new HttpClient();
+        }
+
+        public string GetIndex(int? categoryId)
+        {
+            var _url = UriCreater.GetIndexUrl(categoryId);
+
+            var _response = this._client.GetAsync(_url).Result;
+
+            if (_response.IsSuccessStatusCode == true)
+            {
+                return _response.Content.ReadAsStringAsync().Result;
+            }
+
+            return string.Empty;
+        }
+
+        public string GetChannel(DateTime? date, int? channelId)
+        {
+            var _url = UriCreater.GetChannelUrl(date, channelId);
+
+            var _response = this._client.GetAsync(_url).Result;
+
+            if(_response.IsSuccessStatusCode == true)
+            {
+                return _response.Content.ReadAsStringAsync().Result;
+            }
+
+            return string.Empty;
+        }
+        
+    }
+}
